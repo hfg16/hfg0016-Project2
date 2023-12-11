14381e2d23acfe64dd9cd6ff9e07ec6f89cf5ed9
hfg16 <135649899+hfg16@users.noreply.github.com>
2023-12-10 20:59:35 -0600

    Final_Update project2.py
    Got 60 of 65 possibole points

--- project2.py
+++ project2.py
@@ -1,7 +1,6 @@
 #created by: hfg0016
 
 
-import sqlite3
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
@@ -12,28 +11,168 @@ import jwt
 import datetime
 import sqlite3
 
-connection = sqlite3.connect("totally_not_my_privateKeys.db")
+hostName = "localhost"
+serverPort = 8080
+DATABASE_NAME = "totally_not_my_privateKeys.db"
+
+
+# Create a SQLite database and connect to it
+def initialize_database():
+    conn = sqlite3.connect(DATABASE_NAME)
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS keys (
+            kid INTEGER PRIMARY KEY AUTOINCREMENT,
+            key BLOB NOT NULL,
+            exp INTEGER NOT NULL
+        )
+    """)
+    conn.commit()
+    conn.close()
+
+
 
 private_key = rsa.generate_private_key(
     public_exponent=65537,
     key_size=2048,
 )
-
-plaintext = private_key.decrypt(
-        cyphertext,
-        padding.OAEP()
+expired_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048,
 )
 
-cursor = connection.cursor()
+pem = private_key.private_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PrivateFormat.TraditionalOpenSSL,
+    encryption_algorithm=serialization.NoEncryption()
+)
+expired_pem = expired_key.private_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PrivateFormat.TraditionalOpenSSL,
+    encryption_algorithm=serialization.NoEncryption()
+)
 
-cursor.execute("""CREATE TABLE IF NOT EXISTS keys(
-    kid INTEGER PRIMARY KEY AUTOINCREMENT,
-    key BLOB NOT NULL,
-    exp INTEGER NOT NULL
-)""");
+numbers = private_key.private_numbers()
 
-connection.commit()
+def save_key_to_db(pem, expiration):
+    conn = sqlite3.connect(DATABASE_NAME)
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO keys (key, exp) VALUES (?, ?)", (pem, expiration))
+    conn.commit()
+    conn.close()
 
-connection.close()
+def get_key_from_db(expired=False):
+    conn = sqlite3.connect(DATABASE_NAME)
+    cursor = conn.cursor()
 
-print(plaintext)
+    if expired:
+        cursor.execute("SELECT key FROM keys WHERE exp <= ?", (int(datetime.datetime.utcnow().timestamp()),))
+    else:
+        cursor.execute("SELECT key FROM keys WHERE exp > ?", (int(datetime.datetime.utcnow().timestamp()),))
+
+    result = cursor.fetchone()
+    conn.close()
+
+    if result:
+        return result[0]
+
+    return None
+
+def int_to_base64(value):
+    """Convert an integer to a Base64URL-encoded string"""
+    value_hex = format(value, 'x')
+    # Ensure even length
+    if len(value_hex) % 2 == 1:
+        value_hex = '0' + value_hex
+    value_bytes = bytes.fromhex(value_hex)
+    encoded = base64.urlsafe_b64encode(value_bytes).rstrip(b'=')
+    return encoded.decode('utf-8')
+
+initialize_database()
+# Generate and store at least one key that expires now or less
+save_key_to_db(expired_pem, int(datetime.datetime.utcnow().timestamp()))
+
+# Generate and store at least one key that expires in 1 hour or more
+save_key_to_db(pem, int((datetime.datetime.utcnow()+ datetime.timedelta(hours=1)).timestamp()))
+
+
+
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_PUT(self):
+        self.send_response(405)
+        self.end_headers()
+        return
+
+    def do_PATCH(self):
+        self.send_response(405)
+        self.end_headers()
+        return
+
+    def do_DELETE(self):
+        self.send_response(405)
+        self.end_headers()
+        return
+
+    def do_HEAD(self):
+        self.send_response(405)
+        self.end_headers()
+        return
+
+    def do_POST(self):
+        parsed_path = urlparse(self.path)
+        params = parse_qs(parsed_path.query)
+        if parsed_path.path == "/auth":
+            headers = {
+                "kid": "goodKID"
+            }
+            token_payload = {
+                "user": "username",
+                "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+            }
+            if 'expired' in params:
+                headers["kid"] = "expiredKID"
+                token_payload["exp"] = datetime.datetime.utcnow() - datetime.timedelta(hours=1)
+            encoded_jwt = jwt.encode(token_payload, get_key_from_db(expired=False), algorithm="RS256", headers=headers)
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(bytes(encoded_jwt, "utf-8"))
+            return
+
+        self.send_response(405)
+        self.end_headers()
+        return
+
+    def do_GET(self):
+        if self.path == "/.well-known/jwks.json":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            keys = {
+                "keys": [
+                    {
+                        "alg": "RS256",
+                        "kty": "RSA",
+                        "use": "sig",
+                        "kid": "goodKID",
+                        "n": int_to_base64(numbers.public_numbers.n),
+                        "e": int_to_base64(numbers.public_numbers.e),
+                    }
+                ]
+            }
+            self.wfile.write(bytes(json.dumps(keys), "utf-8"))
+            return
+
+        self.send_response(405)
+        self.end_headers()
+        return
+
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
